cc432b8b0796c891891d58d39f065ff14430a8c3
varsharoshan <96183365+varsharoshan@users.noreply.github.com>
2022-05-14 12:01:27 +0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "pv20varsha@gmail.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+" > ~/.streamlit/config.toml
